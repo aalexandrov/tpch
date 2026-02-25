5bd8434f74e57f7c5b4f25173fcef841510a3f05
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2026-02-25 19:59:09 +0200

    fix: make psql data loading work from any folder

--- scripts/psql/02_data.sql
+++ scripts/psql/02_data.sql
@@ -2,11 +2,14 @@
 
 SET search_path = :schema;
 
-\COPY region FROM './data/sf0001/region.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY nation FROM './data/sf0001/nation.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY supplier FROM './data/sf0001/supplier.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY part FROM './data/sf0001/part.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY customer FROM './data/sf0001/customer.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY partsupp FROM './data/sf0001/partsupp.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY orders FROM './data/sf0001/orders.tbl' WITH (FORMAT csv, DELIMITER '|');
-\COPY lineitem FROM './data/sf0001/lineitem.tbl' WITH (FORMAT csv, DELIMITER '|');
+\set base `echo $HOME/benchmarks/tpch/data/`
+\cd :base:sf
+
+\COPY region FROM './region.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY nation FROM './nation.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY supplier FROM './supplier.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY part FROM './part.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY customer FROM './customer.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY partsupp FROM './partsupp.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY orders FROM './orders.tbl' WITH (FORMAT csv, DELIMITER '|');
+\COPY lineitem FROM './lineitem.tbl' WITH (FORMAT csv, DELIMITER '|');
