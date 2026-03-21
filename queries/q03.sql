359280f7babb57919a35af1d298030a1ab3dc1ec
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2026-03-21 19:06:14 +0200

    chore: fix query parameters for validation

--- queries/q03.sql
+++ queries/q03.sql
@@ -1,9 +1,3 @@
--- $ID$
--- TPC-H/TPC-R Shipping Priority Query (Q3)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
 select
 	l_orderkey,
 	sum(l_extendedprice * (1 - l_discount)) as revenue,
@@ -26,4 +20,4 @@ group by
 order by
 	revenue desc,
 	o_orderdate;
-:n 10
+
--- queries/q15.sql
+++ queries/q15.sql
@@ -1,40 +1,32 @@
--- $ID$
--- TPC-H/TPC-R Top Supplier Query (Q15)
--- Functional Query Definition
--- Approved February 1998
-:x
-create view revenue:s (supplier_no, total_revenue) as
-	select
-		l_suppkey,
-		sum(l_extendedprice * (1 - l_discount))
-	from
-		lineitem
-	where
-		l_shipdate >= date ':1'
-		and l_shipdate < date ':1' + interval '3' month
-	group by
-		l_suppkey;
-
-:o
-select
-	s_suppkey,
-	s_name,
-	s_address,
-	s_phone,
-	total_revenue
-from
-	supplier,
-	revenue:s
-where
-	s_suppkey = supplier_no
-	and total_revenue = (
+with 
+	revenue as (
 		select
-			max(total_revenue)
+			l_suppkey as supplier_no,
+			sum(l_extendedprice * (1 - l_discount)) as total_revenue
 		from
-			revenue:s
+			lineitem
+		where
+			l_shipdate >= date '1996-01-01'
+			and l_shipdate < date '1996-01-01' + interval '3' month
+		group by
+			l_suppkey
 	)
-order by
-	s_suppkey;
-
-drop view revenue:s;
-:n -1
+	select
+		s_suppkey,
+		s_name,
+		s_address,
+		s_phone,
+		total_revenue
+	from
+		supplier,
+		revenue
+	where
+		s_suppkey = supplier_no
+		and total_revenue = (
+			select
+				max(total_revenue)
+			from
+				revenue
+		)
+	order by
+		s_suppkey;
