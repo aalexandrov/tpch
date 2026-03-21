4c5655e6153260ee243708874a596b33874fe044
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2026-03-21 17:46:36 +0200

    feat: allow overriding psql parameter defaults

--- scripts/psql/00_params.sql
+++ scripts/psql/00_params.sql
@@ -1,2 +1,6 @@
-\set schema tpch
-\set sf sf0001
+-- Override these by passing them to the psql call:
+--
+-- > psql -v schema=tpch_test -v sf=sf0001 -f file.sql
+--
+\if :{?schema} \else \set schema tpch \endif
+\if :{?sf}     \else \set sf sf0001   \endif
